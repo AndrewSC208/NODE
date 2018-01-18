5d3078e54cff3c0589aad6340741b327221d6a74
Andrew Meiling <ameiling@circadence.com>
2018-01-17 16:07:11 -0800

    Weather updates

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
 const axios = require('axios');
-
+// Config yargs:
 const argv = yargs
     .options({
         address: {
@@ -13,28 +13,25 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-
+// Command args:
 const encodedAddress = encodeURIComponent(argv.address);
 const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-
+// This can be changed with fetch:
 axios.get(geocodeUrl).then((response) => {
     if (response.data.status === 'ZERO_RESULTS' || response.data.error_message) {
         throw new Error('Unable to find address.');
     }
-
     const weatherUrl = `https://api.darksky.net/forecast/49a0d39f8000c7ec1ff0d6f9f30363a1/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}`;
     console.log(response.data.results[0].formatted_address);
-
+    // return a promise to start the next process:
     return axios.get(weatherUrl)
 }).then((response) => {
     const temp = response.data.currently.temperature;
-    const appTemp = response.data.currently.apparentTempurature;
     console.log(temp);
-    console.log(appTemp);
 }).catch((err) => {
     if (err.code === 'ECONNREFUSED') {
         console.log('Unable to connect to API servers.');
     } else {
         console.log(err.message);
     }
-})
+});
